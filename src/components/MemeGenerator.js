7932862761f7a6cabbb4436b069bab049ecaceee
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-20 10:53:55 -0700

    create fetch for api call in MemeGenerator.js

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -6,8 +6,19 @@ class MemeGenerator extends Component {
     this.state = {
       topText: "",
       bottomText: "",
-      randomImage: "http://i.imgflip.com/1bij.jpg"
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((response) => response.json())
+      .then((response) => {
+        const { memes } = response.data;
+        console.log(memes[0]);
+        this.setState({ allMemeImgs: memes });
+      });
   }
 
   render() {
