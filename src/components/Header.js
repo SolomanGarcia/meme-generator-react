23da332d9b9ca71d26a69910d348963133196360
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-20 10:46:18 -0700

    finish Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
         src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
         alt="troll face meme"
       />
+      <p>Meme Generator</p>
     </header>
   );
 }
