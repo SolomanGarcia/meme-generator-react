194ef85c7e3a2b187a4f97cfc100dbaf272f0fd7
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-20 11:12:38 -0700

    bind handleSubmit

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
       allMemeImgs: []
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -26,10 +27,14 @@ class MemeGenerator extends Component {
     this.setState({ [name]: value });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
-        <form className="meme-form">
+        <form className="meme-form" onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="topText"
