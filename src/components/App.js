85e9d4c4924ca94d4ae397a4b4bd44c491602b1c
Soloman Garcia <soloman.garcia@gmail.com>
2021-01-20 10:43:54 -0700

    create Header and Memegenerator components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Header from "../components/Header";
+import MemeGenerator from "../components/MemeGenerator";
 
 function App() {
-  return <h1>Hello World!!!</h1>;
+  return (
+    <div>
+      <Header />
+      <MemeGenerator />
+    </div>
+  );
 }
 
 export default App;
